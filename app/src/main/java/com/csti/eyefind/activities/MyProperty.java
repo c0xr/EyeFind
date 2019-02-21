cc57044c2d4ad015ff633e5ce7f752bafbc65bd1
LonglyWolf <814484626@qq.com>
2019-02-21 23:07:31 +0800

    Yuanhao:
    two days of my work.
    
    add: MyProperty MyLostFragment MyGetFragment
     MyPropertyContent.
    
    all of these at the bottomNav&2.

---
+++ app/src/main/java/com/csti/eyefind/activities/MyProperty.java
@@ -0,0 +1,91 @@
+package com.csti.eyefind.activities;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.csti.eyefind.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyProperty extends AppCompatActivity {
+
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    private LayoutInflater mInflater;
+    private List<String> mTitleList = new ArrayList<>();//页卡标题集合
+    private View view1, view2, view3, view4, view5;//页卡视图
+    private List<View> mViewList = new ArrayList<>();//页卡视图集合
+    private List<String> listTitles;
+    private List<Fragment> fragments;
+    private List<TextView> listTextViews;
+
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_property);
+        mViewPager = (ViewPager) findViewById(R.id.vp_view);
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        initData();
+        mTabLayout.setTabMode (TabLayout.MODE_FIXED);//平均分配铺满
+
+
+    }
+    private void initData() {
+        listTitles = new ArrayList<>();
+        fragments = new ArrayList<>();
+        listTextViews = new ArrayList<>();
+
+        listTitles.add("我捡的宝贝");
+        listTitles.add("我丢的宝贝");
+
+        for (int i = 0; i < listTitles.size(); i++) {
+            Fragment fragment = MyPropertyContentFragment.newInstance(i,listTitles.get(i));
+            fragments.add(fragment);
+        }
+        //mTabLayout.setTabMode(TabLayout.SCROLL_AXIS_HORIZONTAL);//设置tab模式，当前为系统默认模式
+        for (int i=0;i<listTitles.size();i++){
+            mTabLayout.addTab(mTabLayout.newTab().setText(listTitles.get(i)));//添加tab选项
+        }
+
+        FragmentPagerAdapter mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+            //ViewPager与TabLayout绑定后，这里获取到PageTitle就是Tab的Text
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return listTitles.get(position);
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);//将TabLayout和ViewPager关联起来。
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);//给Tabs设置适配器
+
+    }
+
+}
