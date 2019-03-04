f322d5268e94563887d59c9cb0119031f53debae
LonglyWolf <814484626@qq.com>
2019-03-04 11:11:31 +0800

    Fish
    所有碎片搭建结束

--- app/src/main/java/com/csti/eyefind/activities/MainActivity.java
+++ app/src/main/java/com/csti/eyefind/activities/MainActivity.java
@@ -1,23 +1,46 @@
 package com.csti.eyefind.activities;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.csti.eyefind.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cn.bmob.push.BmobPush;
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobInstallation;
+import cn.bmob.v3.BmobInstallationManager;
+import cn.bmob.v3.BmobPushManager;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.InstallationListener;
+import cn.bmob.v3.datatype.BmobPointer;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.PushListener;
+import cn.bmob.v3.listener.QueryListener;
+import cn.bmob.v3.listener.SaveListener;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -50,31 +73,43 @@ public class MainActivity extends AppCompatActivity {
     private FragmentTransaction transaction ;
     private Fragment primeFragment ;
     private Fragment myFragment ;
+    private Fragment homePageFragment;
+    private Fragment LogInFragment;
 
     private TextView mTextMessage;
-    private String mUserAccount;//用户账号//学号
+    private String objectId;//用户账号//学号
+
+//    Handler handler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//            if (msg.what == 0x0){
+//                transaction.hide(myFragment).hide(primeFragment).hide(homePageFragment).show(LogInFragment).commit();
+//            }
+//        }
+//    };
+
+    private Person person;
+    private BottomNavigationView navigation;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    transaction.hide(myFragment).show(primeFragment).commit();
+                    transaction.hide(myFragment).hide(homePageFragment).hide(LogInFragment).show(primeFragment).commit();
 //                    replace(new MainPrimeFragment());
                     return true;
                 case R.id.navigation_dashboard:
                     if(BmobUser.isLogin()) {
-                        transaction.hide(primeFragment).show(myFragment).commit();
+                        transaction.hide(primeFragment).hide(homePageFragment).hide(LogInFragment).show(myFragment).commit();
                     }else {
                         com.csti.eyefind.activities.I_pick_thing.showDialog("请登录!", null, MainActivity.this);
                     }
                     return true;
                 case R.id.navigation_notifications:
-                    SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
-                    final String objectId = preferences.getString("objectId", "000");
                     //查找Person表里面id为3533107f3e的数据
                     BmobQuery<Person> bmobQuery = new BmobQuery<Person>();
                     bmobQuery.getObject(objectId, new QueryListener<Person>() {
@@ -82,15 +117,17 @@ public class MainActivity extends AppCompatActivity {
                         public void done(Person object, BmobException e) {
                             if (e == null) {
                                 //进用户页面
-                                Intent intent = new Intent(MainActivity.this, UserActivity.class);
-                                intent.putExtra("objectId", objectId);
-                                startActivity(intent);
+                                transaction.hide(myFragment).hide(primeFragment).hide(LogInFragment).show(homePageFragment).commit();
+//                                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+//                                intent.putExtra("objectId", objectId);
+//                                startActivity(intent);
                                 //Toast.makeText(MainActivity.this,"查询成功",Toast.LENGTH_SHORT).show();
                             } else {
                                 //进登录页面
+                                transaction.hide(myFragment).hide(primeFragment).hide(homePageFragment).show(LogInFragment).commit();
                                 //Toast.makeText(MainActivity.this,"查询失败：" + e.getMessage(),Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                                startActivity(intent);
+//                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                                startActivity(intent);
                             }
                         }
                     });
@@ -105,18 +142,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+        objectId = preferences.getString("objectId", "000");
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         replace();
+
     }
 
     private void replace() {
         transaction = getSupportFragmentManager().beginTransaction();
         primeFragment = new MainPrimeFragment();
         myFragment = new MainMyPropertyFragment();
+        homePageFragment = new MyHomePageFragment();
+        LogInFragment = new MyLogInFragment();
         transaction.add(R.id.main_fragment,primeFragment);
         transaction.add(R.id.main_fragment,myFragment);
-        transaction.hide(myFragment).show(primeFragment).commit();
+        transaction.add(R.id.main_fragment,homePageFragment);
+        transaction.add(R.id.main_fragment,LogInFragment);
+        transaction.hide(myFragment).hide(homePageFragment).hide(LogInFragment).show(primeFragment).commit();
     }
+
+    public String getObjectId(){
+        return objectId;
+    }
+
+    public void setObjectId(String newObjectId){
+        objectId = newObjectId;
+    }
+
+    public void changeToPrimeFragment(){
+        ((MyHomePageFragment)homePageFragment).saveIfo(objectId);
+        navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    public void changeToHomePageFragment(){
+        ((MyHomePageFragment) homePageFragment).changeUserViewIfo(getPerson(), objectId);
+        navigation.setSelectedItemId(R.id.navigation_notifications);
+    }
+
+    public void changeToLogInFragment(){
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+        objectId = preferences.getString("objectId", "000");
+        navigation.setSelectedItemId(R.id.navigation_notifications);
+    }
+
+    public void setPerson(Person newPerson){
+        person = newPerson;
+    }
+
+    public Person getPerson(){
+        return person;
+    }
+
 }
--- app/src/main/java/com/csti/eyefind/activities/MyLogInFragment.java
+++ app/src/main/java/com/csti/eyefind/activities/MyLogInFragment.java
@@ -1,24 +1,50 @@
 package com.csti.eyefind.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.csti.eyefind.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MyLogInFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MyLogInFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Arrays;
+import java.util.List;
+
+import cn.bmob.v3.BmobInstallation;
+import cn.bmob.v3.BmobInstallationManager;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.InstallationListener;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.SaveListener;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
 public class MyLogInFragment extends Fragment {
+
+    private EditText account;
+    private EditText password;
+    private Button Login;
+    private Button register;
+    private CheckBox checkBox;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private boolean isremember;
+    private String s_account;
+    private String s_password;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,7 +91,100 @@ public class MyLogInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_log_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_log_in, container, false);
+        account = view.findViewById(R.id.account);
+        password = view.findViewById(R.id.password);
+        checkBox = view.findViewById(R.id.remember_pass);
+        Login = view.findViewById(R.id.login);
+        register = view.findViewById(R.id.register);
+        sharedPreferences = getActivity().getSharedPreferences("data", MODE_PRIVATE);
+        isremember = sharedPreferences.getBoolean("isremember", false);
+        if (isremember) {
+            s_account = sharedPreferences.getString("account", "");
+            s_password = sharedPreferences.getString("password", "");
+            account.setText(s_account);
+            password.setText(s_password);
+            checkBox.setChecked(true);
+        }
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Person user = new Person();
+                //此处替换为你的用户名
+                user.setUsername(account.getText().toString());
+                //此处替换为你的密码
+                user.setPassword(password.getText().toString());
+                user.login(new SaveListener<Person>() {
+                    @Override
+                    public void done(Person bmobUser, BmobException e) {
+                        if (e == null) {
+                            Person user = BmobUser.getCurrentUser(Person.class);
+                            Toast.makeText(getActivity(),"欢迎回来"+user.getmName(),Toast.LENGTH_SHORT).show();
+
+                            BmobQuery<LostItem> query = new BmobQuery<>();
+                            query.addWhereEqualTo("mPerson", user);
+                            query.findObjects(new FindListener<LostItem>() {
+                                @Override
+                                public void done(List<LostItem> object, BmobException e) {
+                                    if(e==null){
+                                        //Toast.makeText(LoginActivity.this,"成功"+object.size(),Toast.LENGTH_SHORT).show();
+                                        String[] Lostthing = new String[object.size()];
+                                        for(int i=0;i<object.size();i++){
+                                            Lostthing[i]=object.get(i).getLabel();
+                                        }
+                                        BmobInstallationManager.getInstance().subscribe(Arrays.asList(Lostthing), new InstallationListener<BmobInstallation>() {
+                                            @Override
+                                            public void done(BmobInstallation bmobInstallation, BmobException e) {
+                                                if (e == null) {
+                                                    //Toast.makeText(LoginActivity.this, "批量订阅成功", Toast.LENGTH_SHORT).show();
+
+                                                } else {
+                                                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+
+
+                                    }else{
+                                        Toast.makeText(getActivity(),"失败",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+
+                            editor=sharedPreferences.edit();
+                            if (checkBox.isChecked()) {
+                                editor.putString("account",user.getUsername());/////////////////////////////////
+                                editor.putString("password",user.getmPassword());
+                                editor.putBoolean("isremember",true);
+                            }else {
+                                editor.clear();
+                            }
+                            editor.apply();
+                            MainActivity activity = (MainActivity)getActivity();
+                            activity.setObjectId(user.getObjectId());
+                            activity.setPerson(user);
+                            activity.changeToHomePageFragment();
+                        } else {
+                            Toast.makeText(getActivity(),"失败"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                String objectId = ((MainActivity)getActivity()).getObjectId();
+                SharedPreferences.Editor editor = getActivity().getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("objectId",objectId);
+                editor.apply();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
